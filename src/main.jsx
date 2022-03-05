af0576060e789be800a457cbbbe6e0766cc912bf
Aoufi Abderahmane <aoufi.abderahmane@lastingdynamics.com>
2022-03-05 09:48:30 +0100

    setup tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css"
 
 ReactDOM.render(
-  <React.StrictMode></React.StrictMode>,
+  <React.StrictMode>
+    <p>tail</p>
+  </React.StrictMode>,
   document.getElementById("root")
 );
